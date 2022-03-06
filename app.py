22d5e9cc65da4cf831c6e7b7bd94f7336da5f861
Fady Missiha <fady.missiha@gmail.com>
2022-03-06 00:44:42 -0500

    thrird
    correction

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import sys
-!{sys.executable} -m pip install -r req.txt --quiet
+!pip install -r req.txt --quiet
 !FLASK_ENV=development FLASK_APP=wsfraud.py flask run
 !curl http://localhost:5000/status
